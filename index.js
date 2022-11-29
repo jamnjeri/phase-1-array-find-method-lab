ac480d78bb2d0141706e309625372db3083e759b
Jamila <jamnjeri@gmail.com>
2022-11-29 12:54:37 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// code your solution here
+// // // code your solution here
+// const record = [
+//     {year: "2015", result: "W"},
+//     {year: "2014", result: "N/A"},
+//     {year: "2013", result: "L"}
+// ]
+
+function superbowlWin(record){
+    //return record.result === "W"
+    const res = record.find(record=>record.result ==="W")
+
+    if(!res){
+        return undefined
+    }   
+    return res.year
+
+}
+
+// console.log(superbowlWin(record));
+
+
+
+
+
